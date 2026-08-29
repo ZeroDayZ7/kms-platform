68863edf3da35db955c521481a5237548eec00ea
ZeroDayZ7 <ZeroDayZ7@proton.me>
2026-08-30 00:20:56 +0200

    feat: update dependencies and remove unused packages across multiple crates

--- crates/kms-service/src/domain/audit/models.rs
+++ crates/kms-service/src/domain/audit/models.rs
@@ -1,4 +1,3 @@
-// src/domain/audit/models.rs
 use crate::domain::keys::models::{KeyAlgorithm, ServiceId};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
@@ -29,7 +28,6 @@ pub enum AuditStatus {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AuditLog {
-    #[serde(with = "bson::serde_helpers::uuid_1_as_binary")]
     pub id: uuid::Uuid,
     pub caller_service: ServiceId,
     pub target_service: ServiceId,
@@ -37,7 +35,5 @@ pub struct AuditLog {
     pub algorithm: KeyAlgorithm,
     pub status: AuditStatus,
     pub reason: Option<String>,
-
-    #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
     pub timestamp: DateTime<Utc>,
 }
