e351dca6f4b765d8db73442bf01a6b0f0d7baef5
ZeroDayZ7 <ZeroDayZ7@proton.me>
2026-09-02 11:11:04 +0200

    feat(logger): enhance logging configuration with default filter and improved format handling

--- crates/kms-service/src/server/logger.rs
+++ crates/kms-service/src/server/logger.rs
@@ -4,12 +4,17 @@ use tracing_subscriber::{EnvFilter, Layer};
 
 use crate::config::{LogConfig, LogFormat};
 
-//#region init_logging
 pub fn init_logging(config: &LogConfig) {
+    // Wyciszamy szum z bibliotek sieciowych i frameworka webowego (axum/hyper/tower)
+    let default_filter = format!(
+        "{},hyper=info,reqwest=info,h2=info,tower_http=info,axum=info",
+        config.level.as_ref()
+    );
     let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(config.level.as_ref()));
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(default_filter));
 
     let console_layer = match config.format {
+        // Produkcyjny JSON bez zmian
         LogFormat::Json => tracing_subscriber::fmt::layer()
             .json()
             .with_span_list(false)
@@ -17,19 +22,18 @@ pub fn init_logging(config: &LogConfig) {
             .with_timer(tracing_subscriber::fmt::time::ChronoLocal::rfc_3339())
             .with_target(false)
             .boxed(),
-        LogFormat::Compact => tracing_subscriber::fmt::layer()
+
+        // Kompaktowy log konsolowy z czasem HH:MM:SS i bez odstępów
+        LogFormat::Compact | LogFormat::Pretty => tracing_subscriber::fmt::layer()
             .compact()
             .with_writer(std::io::stdout)
-            .with_timer(tracing_subscriber::fmt::time::ChronoLocal::rfc_3339())
+            .with_timer(tracing_subscriber::fmt::time::ChronoLocal::new(
+                "%H:%M:%S".to_string(),
+            ))
             .with_ansi(true)
             .with_target(false)
-            .boxed(),
-        LogFormat::Pretty => tracing_subscriber::fmt::layer()
-            .pretty()
-            .with_writer(std::io::stdout)
-            .with_timer(tracing_subscriber::fmt::time::ChronoLocal::rfc_3339())
-            .with_ansi(true)
-            .with_target(false)
+            .with_file(false)
+            .with_line_number(false)
             .boxed(),
     };
 
